3976180d8a5f8401427880412c0b00f917bfba9e
Sachiterasu <teirishwolf@gmail.com>
2024-09-25 17:47:20 -0700

    Object pool and observer

---
+++ Assets/Scripts/FSM/JumpingState.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingState : HeroineState
+{
+    public virtual void handleInput(Heroine heroine, Input input) { }
+}
