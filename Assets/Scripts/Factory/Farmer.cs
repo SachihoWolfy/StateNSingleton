8e38fb80daa17e3f8331577d2d27060afacebc30
Sachiterasu <teirishwolf@gmail.com>
2024-10-12 18:38:38 -0700

    Small fix

--- Assets/Scripts/Factory/Farmer.cs
+++ Assets/Scripts/Factory/Farmer.cs
@@ -11,18 +11,18 @@ public class Farmer : INPC
         switch ((index - 1) % 6)
         {
             case 0:
-                return "Mountain Farmer: Ah, hello there.";
+                return "Mountain Farmer:\nAh, hello there.";
             case 1:
-                return "Mountain Farmer: You're probably wondering how I grow anything here with it always being night here.";
+                return "Mountain Farmer:\nYou're probably wondering how I grow anything here with it always being night here.";
             case 2:
-                return "Mountain Farmer: Well, it's simple.";
+                return "Mountain Farmer:\nWell, it's simple.";
             case 3:
-                return "Mountain Farmer: The Moon here is bright enough to allow crops to grow.";
+                return "Mountain Farmer:\nThe Moon here is bright enough to allow crops to grow.";
             case 4:
-                return "Mountain Farmer: It's slightly chilly, but that's mostly the elevation.";
+                return "Mountain Farmer:\nIt's slightly chilly, but that's mostly the elevation.";
             case 5:
-                return "Mountain Farmer: It's warm enough to grow crops!";
+                return "Mountain Farmer:\nIt's warm enough to grow crops!";
         }
-        return "Mountain Farmer: Don't mind me.";
+        return "Mountain Farmer:\nDon't mind me.";
     }
 }
